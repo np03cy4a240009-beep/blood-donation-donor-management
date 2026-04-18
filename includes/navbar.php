cb477ed1d52c15956d7f99a82fb04b843427d628
darpangc01 <404tryagainerr@gmail.com>
2026-04-18 19:06:18 +0545

    Navbar component

---
+++ includes/navbar.php
@@ -0,0 +1,14 @@
+<header class="landing-header">
+    <div class="logo-wrap">
+        <img src="assets/images/logo.png" alt="logo">
+        <div class="logo-text">
+            <h2>BloodLine Home</h2>
+            <p>Saving Lives Together</p>
+        </div>
+    </div>
+
+    <div class="nav-actions">
+        <a href="login.php">Login</a>
+        <a href="register.php" class="btn btn-primary">Register</a>
+    </div>
+</header>
